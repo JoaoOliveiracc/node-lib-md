cc83f159f6c17e6da0ff17d872fd5b3823121b25
JoaoOliveiracc <ccjoao_monteiro@outlook.com>
2024-03-14 19:43:34 -0300

    using fs

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log(chalk.blue('Hello World!'));
+function getFile(filePath) {
+    const encode = 'UTF-8';
+    fs.readFile(filePath, encode, (_, text) => {
+        console.log(chalk.green(text));
+    })
+}
+
+getFile('./arquivos/texto.md');
