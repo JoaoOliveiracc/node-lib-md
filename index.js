d442fb2bc790360acc983a352f32ad3407d0a921
JoaoOliveiracc <ccjoao_monteiro@outlook.com>
2024-03-18 18:09:49 -0300

    add promisses

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+function trateErro(err) {
+    throw new Error(chalk.red(err.code, 'Não há arquivo no diretório'))
+}
+
 function getFile(filePath) {
     const encode = 'UTF-8';
-    fs.readFile(filePath, encode, (_, text) => {
-        console.log(chalk.green(text));
-    })
+    fs.promises
+        .readFile(filePath, encode)
+        .then((text) => console.log(chalk.green(text)))
+        .catch(trateErro)
 }
 
 getFile('./arquivos/texto.md');
