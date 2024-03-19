ba141cde9282135d31c8a6ad3e55e139b490947b
JoaoOliveiracc <ccjoao_monteiro@outlook.com>
2024-03-19 12:55:59 -0300

    add promisses

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
-function trateErro(err) {
-    throw new Error(chalk.red(err.code, 'Não há arquivo no diretório'))
+function trateErro(error) {
+    throw new Error(chalk.red(error.code, 'Não há arquivo no diretório'))
 }
 
-function getFile(filePath) {
-    const encode = 'UTF-8';
-    fs.promises
-        .readFile(filePath, encode)
-        .then((text) => console.log(chalk.green(text)))
-        .catch(trateErro)
+async function getFile(filePath) {
+    try {
+        const encode = 'UTF-8';
+        const text = await fs.promises.readFile(filePath, encode)
+        console.log(chalk.green(text))
+    } catch (error) {
+        trateErro(error)
+    }
 }
 
 getFile('./arquivos/texto.md');
