b4451b51544cca15427b980fde5ba2ff2091882d
JoaoOliveiracc <ccjoao_monteiro@outlook.com>
2024-03-24 12:26:09 -0300

    process dir

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,24 @@
 import getFile from "./index.js";
+import fs from 'fs';
 import chalk from "chalk";
 
 const path = process.argv;
 
 getFile(path[2]);
 
-async function textProcessing(path) {
-    const result = await getFile(path[2]);
-    console.log(result);
+async function textProcessing(args) {
+    const path = args[2];
+
+    if (fs.lstatSync(path).isFile()) {
+        const result = await getFile(path);
+        console.log(result);
+    } else if (fs.lstatSync(path).isDirectory()) {
+        const files = await fs.promises.readdir(path);
+        files.forEach(async(fileName) => {
+            const list = await getFile(`${path}/${fileName}`);
+
+        })
+    }
 }
 
 textProcessing(path)
