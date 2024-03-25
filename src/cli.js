d924de16a5648921a6b8fa7ec536dd49c9902b14
JoaoOliveiracc <ccjoao_monteiro@outlook.com>
2024-03-25 19:48:39 -0300

    add script

--- src/cli.js
+++ src/cli.js
@@ -1,22 +1,36 @@
 import getFile from "./index.js";
 import fs from 'fs';
-import chalk from "chalk";
+import chalk from 'chalk';
 
 const path = process.argv;
 
-getFile(path[2]);
+function printList(result, identifier = '') {
+    console.log(
+        chalk.yellow("Lista de URL's",
+        chalk.black.bgGreen(identifier),
+        result)
+    );
+}
 
 async function textProcessing(args) {
     const path = args[2];
 
+    try {
+        fs.lstatSync(path);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.log('Path or file not found!');
+        }
+    }
+
     if (fs.lstatSync(path).isFile()) {
         const result = await getFile(path);
-        console.log(result);
+        printList(result);
     } else if (fs.lstatSync(path).isDirectory()) {
         const files = await fs.promises.readdir(path);
         files.forEach(async(fileName) => {
             const list = await getFile(`${path}/${fileName}`);
-
+            printList(list, fileName);
         })
     }
 }
